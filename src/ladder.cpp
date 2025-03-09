557e8a30dba8367749e981de3fb087bbaba9a44e
NautilusWu <149129586+NautilusWu@users.noreply.github.com>
2025-03-09 14:58:26 +0800

    Add files via upload

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -55,39 +55,42 @@ vector<string> generate_word_ladder(const string& begin_word, const string& end_
         cout << word << endl;}
 
 
-    queue<vector<string>> ladder_queue;
-    set<string> visited;
-    set<string> word_list_copy(word_list.begin(), word_list.end());
+    // queue<vector<string>> ladder_queue;
+    // set<string> visited;
+    // set<string> word_list_copy(word_list.begin(), word_list.end());
 
-    ladder_queue.push({begin_word});
-    visited.insert(begin_word);
-    while (!ladder_queue.empty()) {
-        vector<string> ladder = ladder_queue.front();
-        ladder_queue.pop();
-        string last_word = ladder.back();
-        for (auto it = word_list_copy.begin(); it != word_list_copy.end(); ) {
-            const string& word = *it;
-            if (is_adjacent(last_word, word)) {
-                if (visited.find(word) == visited.end()) {
-                    visited.insert(word);
-                    vector<string> new_ladder = ladder;
-                    new_ladder.push_back(word);
-                    if (word == end_word) {
-                        return new_ladder;
-                    }
-                    ladder_queue.push(new_ladder);
-                }
-                it = word_list_copy.erase(it);
-            } else {
-                ++it;
-            }
-        }
-    }
+    // ladder_queue.push({begin_word});
+    // visited.insert(begin_word);
+    // while (!ladder_queue.empty()) {
+    //     vector<string> ladder = ladder_queue.front();
+    //     ladder_queue.pop();
+    //     string last_word = ladder.back();
+    //     for (auto it = word_list_copy.begin(); it != word_list_copy.end(); ) {
+    //         const string& word = *it;
+    //         if (is_adjacent(last_word, word)) {
+    //             if (visited.find(word) == visited.end()) {
+    //                 visited.insert(word);
+    //                 vector<string> new_ladder = ladder;
+    //                 new_ladder.push_back(word);
+    //                 if (word == end_word) {
+    //                     return new_ladder;
+    //                 }
+    //                 ladder_queue.push(new_ladder);
+    //             }
+    //             it = word_list_copy.erase(it);
+    //         } else {
+    //             ++it;
+    //         }
+    //     }
+    // }
     return {};
 }
 
 // Load words from file
 void load_words(set<string>& word_list, const string& file_name) {
+    word_list.clear();
+
+
     ifstream file(file_name);
     if (!file.is_open()) {
         cerr << "Cannot open file: " << file_name << endl;
